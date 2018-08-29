ee985196b482aa23fa89b6942ac8141ce370322d
dongqizhen <960384776@qq.com>
2018-08-29 11:31:44 +0800

    团购详情

--- src/module/groupBuying/page/GroupDetails.vue
+++ src/module/groupBuying/page/GroupDetails.vue
@@ -68,7 +68,7 @@
                             <span></span>
                         </h2>
                         <span>医院用户查看</span>
-                        <ul>
+                        <ul class="nav">
                             <touch-ripple :speed="1" :opacity="0.3" color="#ccc" transition="ease">
                                 <li class="active">设备团购</li>
                             </touch-ripple>
@@ -80,12 +80,76 @@
                             <li>设备团购</li>
                             <li>设备团购</li>
                         </ul>
-                        <h3>
-                            <p>设备团购产品清单</p>
-                            <span>查看全部 (150款)
-                                <i></i>
-                            </span>
-                        </h3>
+                        <div class="subscription">
+                            <h3>
+                                <p>设备团购产品清单</p>
+                                <span>查看全部 (150款)
+                                    <i></i>
+                                </span>
+                            </h3>
+                            <ul class="type">
+                                <li>CT类
+                                    <span>22款</span>
+                                </li>
+                                <li>核磁共振MRI类
+                                    <span>22款</span>
+                                </li>
+                                <li>血管造影机类
+                                    <span>22款</span>
+                                </li>
+                                <li>CT类
+                                    <span>22款</span>
+                                </li>
+                                <li>CT类
+                                    <span>22款</span>
+                                </li>
+                                <li>CT类
+                                    <span>22款</span>
+                                </li>
+
+                            </ul>
+                        </div>
+                        <div class="brand">
+                            <h2>设备团购入围品牌</h2>
+                            <ul>
+                                <li>
+                                    <span>品牌</span>
+                                    <span>团购占有率</span>
+                                    <span>市场保有率</span>
+                                    <span>售后排名</span>
+                                </li>
+                                <li>
+                                    <span>GE</span>
+                                    <span>35%</span>
+                                    <span>35%</span>
+                                    <span>35%</span>
+                                </li>
+                                <li>
+                                    <span></span>
+                                    <span></span>
+                                    <span></span>
+                                    <span></span>
+                                </li>
+                                <li>
+                                    <span></span>
+                                    <span></span>
+                                    <span></span>
+                                    <span></span>
+                                </li>
+                                <li>
+                                    <span></span>
+                                    <span></span>
+                                    <span></span>
+                                    <span></span>
+                                </li>
+                                <li>
+                                    <span></span>
+                                    <span></span>
+                                    <span></span>
+                                    <span></span>
+                                </li>
+                            </ul>
+                        </div>
                     </div>
                 </div>
             </cube-scroll>
@@ -235,7 +299,7 @@
                     justify-content: center;
                     margin-bottom: 20px;
                 }
-                >ul{
+                >ul.nav{
                     display: flex;
                     padding: 0 13px;
                     flex-wrap: wrap;
@@ -244,9 +308,10 @@
                         width: 100px;
                         height: 31px;
                         border:1px solid #019DDD;
-                        box-shadow: 0 2px 2px 0 rgba(1,157,221,0.15);
+                        
                         border-radius: 31px;
                         display: flex;
+                        border: 1px solid #999999;
                         align-items: center;
                         justify-content: center;
                         font-family: PingFangSC-Regular;
@@ -256,6 +321,112 @@
                         &.active{
                             background: rgba(1,157,221,0.08);
                             color: #019DDD;
+                            border:1px solid #019DDD;
+                            box-shadow: 0 2px 2px 0 rgba(1,157,221,0.15);
+                        }
+                    }
+                }
+                .subscription{
+                    >h3{
+                        display: flex;
+                        justify-content: space-between;
+                        font-family: PingFangSC-Medium;
+                        font-size: 14px;
+                        color: #333333;
+                        height: 50px;
+                        padding: 0 13px;
+                        align-items: center;
+                        span{
+                            font-family: PingFangSC-Regular;
+                            font-size: 12px;
+                            color: #666666;
+                            line-height: 50px;
+                        }
+                    }
+                    >ul.type{
+                        display: flex;
+                        flex-wrap: wrap;
+                        padding: 0 13px;
+                        justify-content: space-between;
+                        li{
+                            height: 50px;
+                            width: 103px;
+                            background: #FFFFFF;
+                            border: 0 solid rgba(1,157,221,0.12);
+                            box-shadow: 1px 2px 4px 1px rgba(1,157,221,0.09);
+                            border-radius: 5px;
+                            margin-bottom: 15px;
+                            display: flex;
+                            flex-direction: column;
+                            font-family: PingFangSC-Medium;
+                            font-size: 12px;
+                            color: #019DDD;
+                            letter-spacing: 0;
+                            text-align: center;
+                            align-items: center;
+                            padding-top: 10px;
+                            span{
+                                font-family: PingFangSC-Regular;
+                                font-size: 10px;
+                                color: #999999;
+                                letter-spacing: 0;
+                                margin-top: 5px;
+                            }
+                        }
+                    }
+                }
+                .brand{
+                    h2{
+                        height: 40px;
+                        margin-bottom: 5px;
+                        padding: 0 13px;
+                        display: flex;
+                        align-items: center;
+                        justify-content: flex-start;
+                        font-family: PingFangSC-Medium;
+                        font-size: 14px;
+                        color: #333333;
+                    }
+                    >ul{
+                        li{
+                            display: flex;
+                            height: 52px;
+                            align-items: center;
+                            border-bottom: 1px solid #E9E9E9;
+                           // justify-content: space-between;
+                            &:first-child{
+                                
+                                height: 36px;
+                                border-top: 1px solid #E9E9E9;
+                                span{
+                                    font-family: PingFangSC-Regular;
+                                    font-size: 12px;
+                                    color: #999999;
+                                }
+                            }
+                            &:last-child{
+                                border-bottom: none;
+                            }
+                            span{
+                                display: flex;
+                                justify-content: center;
+                                font-family: PingFangSC-Medium;
+                                font-size: 14px;
+                                color: #333333;
+                                &:first-child{
+                                    width: 123px;
+                                }
+                                &:nth-child(2){
+                                    width: 60px;
+                                    margin-right: 23px;
+                                }
+                                &:nth-child(3){
+                                    width: 60px;
+                                }
+                                &:last-child{
+                                    width: 83px;
+                                }
+                            }
                         }
                     }
                 }
