bfae1f1baaf59b8bfca1594dded2e24f19ac20ac
lxb <hmlxb001068@163.com>
2018-09-11 10:02:56 +0800

    选择重要参数

---
+++ src/module/groupBuying/page/hospital/mainParams.vue
@@ -0,0 +1,204 @@
+
+<template>
+  <div class="container">
+    <Header :title="this.$route.name">
+       <span slot="explain" class="enter" @click="enterClick">确定</span>
+    </Header>
+    <div class="content">
+      <div class="selectedParamBox">
+          <div class="selectedText">已选参数:</div>
+          <div class="selectedParams">
+             <ul>
+               <li v-for="item in itemSelect" :key="item.id" class="clearfix">
+                   <span class="paramValue">{{item.name}}</span><span class="deleteBtn" @click="deleteItem(item)">X</span>
+               </li>
+             </ul>
+          </div>
+      </div>
+      <search placeholder="请输入重要参数">
+        <!-- //<span slot="explain" @click="save">保存</span> -->
+      </search>
+      <div class="paramBox">
+        <div class="pleaseSelected">请选择:<span>(企业提供的重要参数清单)</span></div>
+        <div class="params">
+          <ul class="clearfix">
+            <li v-for="param in params" :key="param.id" @click="selected(param)" :class="activeClass(param.id)">{{param.name}}</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import Header from "../../components/header/header";
+import search from "../../components/search/search";
+import _ from "lodash";
+import { mapMutations } from "vuex";
+export default {
+  data() {
+    return {
+      params: [
+        { name: "四排", id: 1 },
+        { name: "六排", id: 2 },
+        { name: "其他相关参数", id: 3 },
+        { name: "这应嘎是一个不可知参数性质", id: 4 }
+      ],
+      currentIdx: -1,
+      itemSelect: []
+    };
+  },
+  components: {
+    Header,
+    search
+  },
+  methods: {
+    ...mapMutations(["setTransition"]),
+    enterClick() {
+      this.setTransition("turn-off");
+      this.$router.go(-1);
+    },
+    selected(param) {
+      if (this.itemSelect.length < 3) {
+        if (
+          _.without(this.itemSelect, param).length == this.itemSelect.length
+        ) {
+          this.itemSelect.push(param);
+        } else {
+          this.itemSelect = _.without(this.itemSelect, param);
+        }
+      } else if (this.itemSelect.length == 3) {
+        this.itemSelect = _.without(this.itemSelect, param);
+        if (this.itemSelect.length == 3) {
+          this.showToastTime();
+        }
+      }
+    },
+    deleteItem(item) {
+      this.itemSelect = _.without(this.itemSelect, item);
+    },
+    showToastTime() {
+      const toast = this.$createToast({
+        time: 1000,
+        txt: "最多选择三个型号"
+      });
+      toast.show();
+    },
+    activeClass(id) {
+      if (this.itemSelect) {
+        for (const val of this.itemSelect) {
+          if (val.id == id) {
+            return "active";
+          }
+        }
+      }
+    }
+  }
+};
+</script>
+<style lang="scss" scoped>
+@import "../../../../../static/scss/_commonScss";
+.container {
+  @include basic_container_style;
+  /deep/ header {
+    .search {
+      .enter {
+        padding-right: 13px;
+        font-family: PingFangSC-Regular;
+        font-size: 14px;
+        color: #333333;
+      }
+    }
+  }
+  .content {
+    padding: 13px;
+    .selectedParamBox {
+      background-color: #fff;
+      padding: 10px 13px;
+      margin-bottom: 10px;
+      .selectedText {
+        font-size: 14px;
+        color: #666;
+        margin-bottom: 7px;
+        line-height: 20px;
+      }
+      .selectedParams {
+        ul {
+          display: flex;
+          li {
+            width: 104px;
+            height: 30px;
+            line-height: 30px;
+            border: 1px solid #999999;
+            border-radius: 7px;
+            margin-right: 5px;
+            padding-left: 10px;
+            span {
+              float: left;
+              display: block;
+              text-align: center;
+            }
+            .paramValue {
+              width: 68px;
+              text-overflow: ellipsis;
+              overflow: hidden;
+              white-space: nowrap;
+              text-align: center;
+              font-size: 13px;
+              color: #666;
+            }
+            .deleteBtn {
+              width: 12px;
+              height: 12px;
+              margin: 0 5px;
+              color: #999;
+            }
+          }
+        }
+      }
+    }
+    .paramBox {
+      margin-top: 10px;
+      background-color: #fff;
+      box-shadow: 0 1px 2px 0 rgba(0, 0, 0, 0.1);
+      border-radius: 5px;
+      padding: 10px 13px;
+      .pleaseSelected {
+        font-size: 14px;
+        color: #666;
+        line-height: 20px;
+        margin-bottom: 8px;
+        span {
+          font-size: 12px;
+          color: #ccc;
+          padding-left: 6px;
+        }
+      }
+      .params {
+        ul {
+          width: 102%;
+          li {
+            min-width: 91px;
+            float: left;
+            text-align: center;
+            height: 30px;
+            line-height: 30px;
+            border: 1px solid #999999;
+            border-radius: 7px;
+            margin-right: 10px;
+            margin-bottom: 13px;
+            font-size: 13px;
+            padding: 0 10px;
+            color: #666666;
+            &.active {
+              background: rgba(1, 157, 221, 0.08);
+              border: 1px solid #019ddd;
+              border-radius: 7px;
+              color: #019ddd;
+            }
+          }
+        }
+      }
+    }
+  }
+}
+</style>
--- src/module/groupBuying/page/hospital/perdictTime.vue
+++ src/module/groupBuying/page/hospital/perdictTime.vue
@@ -1,13 +1,15 @@
 <template>
 
   <div class="container">
-    <Header :title="this.$route.name"></Header>
+    <Header :title="this.$route.name">
+      <span slot="explain" class="enter" @click="enterClick">确定</span>
+    </Header>
     <div class="content">
       <ul>
         <li>
           <div class="year">2018年:</div>
           <div class="time clearfix">
-            <span v-for="(time,index) in times" :key="index" :class="nowDate > index?'disabledStyle':''" @click="changeCurrent(index)">{{time}}</span>
+            <span v-for="(time,index) in times" :key="index" :class="[nowDate>=index?'disabledStyle':'',cur===index && nowDate < index?'active':'']" @click="changeCurrent(index)">{{time}}</span>
           </div>
         </li>
         <li>
@@ -25,73 +27,90 @@
 
 </template>
 <script>
-  import Header from "../../components/header/header";
-  export default {
-      data() {
-          return {
-              cur: -1,
-              nowDate: 2,
-              times: ["第一季度", "第二季度", "第三季度", "第四季度"]
-          };
-      },
-      components: {
-          Header
-      },
-      methods: {
-          changeCurrent(index) {
-              this.cur = index;
-          }
-      }
-  };
+import Header from "../../components/header/header";
+import { mapMutations } from "vuex";
+export default {
+  data() {
+    return {
+      cur: -1,
+      nowDate: 1,
+      times: ["第一季度", "第二季度", "第三季度", "第四季度"]
+    };
+  },
+  components: {
+    Header
+  },
+
+  methods: {
+    ...mapMutations(["setTransition"]),
+    changeCurrent(index) {
+      this.cur = index;
+    },
+    enterClick() {
+      this.setTransition("turn-off");
+      this.$router.go(-1);
+    }
+  }
+};
 </script>
 <style lang="scss" scoped>
-  @import "../../../../../static/scss/_commonScss";
-  .container {
-      @include basic_container_style;
-      .content {
-          ul {
-              background-color: #fff;
-              padding: 13px;
-              height: 100%;
-              box-shadow: 0 1px 2px 0 rgba(0, 0, 0, 0.1);
-              border-radius: 5px;
-              li {
-                  margin-bottom: 16px;
-                  .year {
-                      font-size: 14px;
-                      color: #666;
-                      margin-bottom: 8px;
-                  }
-                  .time {
-                      span {
-                          width: 73px;
-                          height: 30px;
-                          line-height: 30px;
-                          display: block;
-                          border: 1px solid #999;
-                          border-radius: 7px;
-                          margin-right: 10px;
-                          text-align: center;
-                          font-size: 13px;
-                          color: #666;
-                          float: left;
-                          &:last-child {
-                              margin-right: 0;
-                          }
-                          &.active {
-                              background: rgba(1, 157, 221, 0.08);
-                              border: 1px solid #019ddd;
-                              border-radius: 7px;
-                          }
-                          &.disabledStyle {
-                              border: 1px solid #cbcbcb;
-                              border-radius: 7px;
-                              color: #cbcbcb;
-                          }
-                      }
-                  }
-              }
-          }
+@import "../../../../../static/scss/_commonScss";
+.container {
+  @include basic_container_style;
+  /deep/ header {
+    .search {
+      .enter {
+        padding-right: 13px;
+        font-family: PingFangSC-Regular;
+        font-size: 14px;
+        color: #333333;
       }
+    }
   }
+  .content {
+    ul {
+      background-color: #fff;
+      padding: 13px;
+      height: 100%;
+      box-shadow: 0 1px 2px 0 rgba(0, 0, 0, 0.1);
+      border-radius: 5px;
+      li {
+        margin-bottom: 16px;
+        .year {
+          font-size: 14px;
+          color: #666;
+          margin-bottom: 8px;
+        }
+        .time {
+          span {
+            width: 73px;
+            height: 30px;
+            line-height: 30px;
+            display: block;
+            border: 1px solid #999;
+            border-radius: 7px;
+            margin-right: 10px;
+            text-align: center;
+            font-size: 13px;
+            color: #666;
+            float: left;
+            &:last-child {
+              margin-right: 0;
+            }
+            &.active {
+              background: rgba(1, 157, 221, 0.08);
+              border: 1px solid #019ddd;
+              border-radius: 7px;
+            }
+            &.disabledStyle {
+              border: 1px solid #cbcbcb;
+              border-radius: 7px;
+              color: #cbcbcb;
+            }
+          }
+        }
+      }
+    }
+  }
+}
 </style>
